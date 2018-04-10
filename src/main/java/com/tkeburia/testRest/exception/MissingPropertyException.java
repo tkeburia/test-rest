c0386d0b5d1decfe3954159edb27c16e54f43df8
Tornike Keburia <tokeburia@deloitte.co.uk>
2018-04-10 17:24:23 +0100

    add travis file

--- src/main/java/com/tkeburia/testRest/exception/MissingPropertyException.java
+++ src/main/java/com/tkeburia/testRest/exception/MissingPropertyException.java
@@ -1,23 +1,7 @@
 package com.tkeburia.testRest.exception;
 
 public class MissingPropertyException extends RuntimeException {
-    public MissingPropertyException() {
-        super();
-    }
-
     public MissingPropertyException(String message) {
         super(message);
-    }
-
-    public MissingPropertyException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public MissingPropertyException(Throwable cause) {
-        super(cause);
-    }
-
-    protected MissingPropertyException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
