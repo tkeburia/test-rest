1f3b1f8a05c0658e4b0c2bceedc862fed031bce8
Tornike Keburia <tokeburia@deloitte.co.uk>
2018-04-18 18:10:17 +0100

    override default message toString() to avoid truncating the text

--- src/main/java/com/tkeburia/testRest/aspect/LoggingAspect.java
+++ src/main/java/com/tkeburia/testRest/aspect/LoggingAspect.java
@@ -4,6 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 import com.tkeburia.testRest.dto.HttpRequestAndOtherArgs;
+import org.apache.activemq.command.ActiveMQMessage;
+import org.apache.activemq.util.ByteArrayInputStream;
+import org.apache.activemq.util.ByteSequence;
+import org.apache.activemq.util.JMSExceptionSupport;
+import org.apache.activemq.util.MarshallingSupport;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -16,8 +21,12 @@ import org.springframework.stereotype.Component;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.InflaterInputStream;
 
 import static com.tkeburia.testRest.util.HttpUtils.getHeaderMap;
 import static com.tkeburia.testRest.util.HttpUtils.separateHttpRequestArgsFromOthers;
@@ -62,9 +71,33 @@ public class LoggingAspect
             argNames = "message"
 
     )
-    public void logMethodData(Message message) throws JMSException, JsonProcessingException {
+    public void logMethodData(ActiveMQMessage message) throws JMSException, JsonProcessingException {
         LOG.info(String.format("Received message on queue %s", message.getJMSDestination().toString()));
-        LOG.info(String.format("Queue Message content : %s", message.toString()));
+        LOG.info(String.format("Queue Message content : %s", message.toString(ImmutableMap.of("text", decodeContent(message.getContent())))));
+    }
+
+    private String decodeContent(ByteSequence bodyAsBytes) throws JMSException {
+        String text = null;
+        if (bodyAsBytes != null) {
+            InputStream is = null;
+            try {
+                is = new ByteArrayInputStream(bodyAsBytes);
+                DataInputStream dataIn = new DataInputStream(is);
+                text = MarshallingSupport.readUTF8(dataIn);
+                dataIn.close();
+            } catch (IOException ioe) {
+                throw JMSExceptionSupport.create(ioe);
+            } finally {
+                if (is != null) {
+                    try {
+                        is.close();
+                    } catch (IOException e) {
+                        // ignore
+                    }
+                }
+            }
+        }
+        return text;
     }
 
 }
