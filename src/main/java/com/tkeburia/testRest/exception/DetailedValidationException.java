ff4a5b487ad881132ced47e980c7742fc389e951
Tornike Keburia <tokeburia@deloitte.co.uk>
2018-04-18 17:50:23 +0100

    modify DetailedValidationException to fetch all validation error messages from the cause

--- src/main/java/com/tkeburia/testRest/exception/DetailedValidationException.java
+++ src/main/java/com/tkeburia/testRest/exception/DetailedValidationException.java
@@ -2,19 +2,17 @@ package com.tkeburia.testRest.exception;
 
 import org.everit.json.schema.ValidationException;
 
+import java.util.HashSet;
 import java.util.Set;
-
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toSet;
 
 public class DetailedValidationException extends RuntimeException {
 
     public DetailedValidationException(ValidationException e) {
-        this(e.getMessage(), e.getCausingExceptions().stream().map(ValidationException::getMessage).collect(toSet()));
+        this(e.getMessage(), new HashSet<>(e.getAllMessages()));
     }
 
     private DetailedValidationException(String baseMessage, Set<String> violations) {
-        this(baseMessage + " " + violations.stream().collect(joining(", ")));
+        this(String.format("%s. %d violations found: %s", baseMessage, violations.size(), violations.toString()));
     }
 
     private DetailedValidationException(String message) {
